e68a383c844e121dd83a3ffeb32f86b1a0dfcb34
inneobr <tecnologiaesolucoespr@gmail.com>
2023-08-16 21:50:21 -0300

    feat: dev

--- src/test/java/org/inneo/services/ServicesApplicationTests.java
+++ src/test/java/org/inneo/services/ServicesApplicationTests.java
@@ -2,10 +2,8 @@ package org.inneo.services;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("prod")
 class ServicesApplicationTests {
 
 	@Test
